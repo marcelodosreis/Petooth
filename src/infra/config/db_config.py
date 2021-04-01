ec33afb6a5cb9877991d2c0cd4093c4367f2fe25
marcelodosreis <marcelohrpaulo13@gmail.com>
2021-04-01 17:57:40 -0300

    feat: creating user model

---
+++ src/infra/config/db_config.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+
+
+class DBConnectionHandler:
+    """ SQLAlchemy database connection """
+
+    def __init__(self):
+        self.__connection_string = "sqlite://storage.db"
+        self.session = None
+
+    def get_engine(self):
+        """Return connection Engine
+        :param - None
+        :return - engine connection to Database
+        """
+        engine = create_engine(self.__connection_string)
+        return engine
