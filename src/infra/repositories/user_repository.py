d9b68e9470264ba378c03e0039a02ce08fd491c8
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2022-06-21 13:54:07 -0300

    feat: implementing interfaces

--- src/infra/repositories/user_repository.py
+++ src/infra/repositories/user_repository.py
@@ -1,11 +1,12 @@
 from typing import List
+from src.data.interfaces import UserRepositoryInterface
 from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Users as UsersModel
 
 
-class UserRepository:
-    """ Class to manage User Repository """
+class UserRepository(UserRepositoryInterface):
+    """Class to manage User Repository"""
 
     @classmethod
     def insert_user(cls, name: str, password: str) -> Users:
@@ -43,15 +44,27 @@ class UserRepository:
 
             if user_id and not name:
                 with DBConnectionHandler() as db_connection:
-                    data = db_connection.session.query(UsersModel).filter_by(id=user_id).one()
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
                     query_data = [data]
             elif not user_id and name:
                 with DBConnectionHandler() as db_connection:
-                    data = db_connection.session.query(UsersModel).filter_by(name=name).one()
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(name=name)
+                        .one()
+                    )
                     query_data = [data]
             elif user_id and name:
                 with DBConnectionHandler() as db_connection:
-                    data = db_connection.session.query(UsersModel).filter_by(id=user_id, name=name).one()
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
                     query_data = [data]
 
             return query_data
